71a335dc6bde7ae34b1d27aee8a615b90d5371d7
momeara2 <momeara2@CompSciICv-45.fsc.int>
2025-03-25 16:30:19 -0400

    Index Test

--- index.php
+++ index.php
@@ -1,16 +1,18 @@
 <!DOCTYPE html>
-<!--
-Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
-Click nbfs://nbhost/SystemFileSystem/Templates/Project/PHP/PHPProject.php to edit this template
--->
 <html>
-    <head>
-        <meta charset="UTF-8">
-        <title></title>
-    </head>
-    <body>
-        <?php
-        // put your code here
-        ?>
-    </body>
+<head>
+  <title>Finance Master - Welcome</title>
+  <link rel="stylesheet" href="https://www.w3schools.com/w3css/4/w3.css">
+  <link rel="stylesheet" href="mystyle.css">
+</head>
+<body class="w3-light-grey">
+
+<div class="w3-container w3-center w3-padding-64">
+  <h1 class="w3-xxlarge">Welcome to Finance Master</h1>
+  <p class="w3-large">Your all-in-one personal banking solution.</p>
+  <a href="register.php" class="w3-button w3-blue w3-margin">Register</a>
+  <a href="login.php" class="w3-button w3-green w3-margin">Login</a>
+</div>
+
+</body>
 </html>
